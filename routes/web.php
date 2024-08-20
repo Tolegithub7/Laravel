fb1fd3ed97f0da9e57ecab1910c43b3f45b09e1e
Tolegithub7 <Tolekadima7@gmail.com>
2024-08-20 17:22:05 +0300

    jobs

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 use Pest\Support\View;
 
@@ -29,9 +30,32 @@ Route::get('/jobs', function () {
         ]]);
 });
 Route::get('/jobs/{id}', function ($id) {
-    dd($id);
+    $jobs = [
+
+            [
+                'id' => 1,
+                'title' => 'bodyguard',
+                'salary' => 90,000,
+                'place' => 'adama'
+            ], [
+                'id' => 2,
+                'title' => 'programmer',
+                'salary' => 70,000,
+                'place' => 'bishoftu'
+            ], [
+                'id' => 3,
+                'title' => 'Driver',
+                'salary' => 80,000,
+                'place' => 'Asella'
+            ]
+        ];
+
+        $job = Illuminate\Support\Arr::first($jobs, fn($job) => $job['id'] == $id);
+        dd($job);
+
     return View('contact');
 });
+
 Route::get('/contact', function () {
     return View('contact');
 });
