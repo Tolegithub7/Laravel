4c6ce5f80a45c9bd147fcbd3586ad4fefd204aa3
Tolegithub7 <Tolekadima7@gmail.com>
2024-08-20 17:55:55 +0300

    last commit

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,12 +2,14 @@
     <x-slot:heading>
         Job List
     </x-slot:heading>
-    @foreach ($jobs as $job)
-        <div class="border p-3 my-2">
-            <a href="">
-                <strong><h3 class="text-lg font-bold">{{ $job['title'] }}</h3></strong>
-                <p>pay : {{ $job['salary'] }}</p> <p>place  : {{ $job['place'] }}</p>
-            </a>
-        </div>
-    @endforeach
+    <ul>
+        @foreach ($jobs as $job)
+            <li>
+                <a href="/jobs/{{ $job['id'] }}">
+                    {{ $job['title'] }} pay : {{ $job['salary'] }} from {{ $job['place'] }}
+                </a>
+            </li>
+        @endforeach
+    </ul>
+
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/', function () {
 });
 Route::get('/jobs', function () {
     return View('jobs', [
-        'jobs' => [
+        $jobs = [
 
             [
                 'id' => 1,
@@ -29,32 +29,32 @@ Route::get('/jobs', function () {
             ]
         ]]);
 });
+
 Route::get('/jobs/{id}', function ($id) {
     $jobs = [
+        [
+            'id' => 1,
+            'title' => 'Developer',
+            'salary' => 70000,
+            'place' => 'Bishoftu'
+        ], [
+            'id' => 2,
+            'title' => 'Designer',
+            'salary' => 80000,
+            'place' => 'Asella'
+        ], [
+            'id' => 3,
+            'title' => 'Driver',
+            'salary' => 60000,
+            'place' => 'Addis Ababa'
+        ]
+    ];
 
-            [
-                'id' => 1,
-                'title' => 'bodyguard',
-                'salary' => 90,000,
-                'place' => 'adama'
-            ], [
-                'id' => 2,
-                'title' => 'programmer',
-                'salary' => 70,000,
-                'place' => 'bishoftu'
-            ], [
-                'id' => 3,
-                'title' => 'Driver',
-                'salary' => 80,000,
-                'place' => 'Asella'
-            ]
-        ];
+    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
 
-        $job = Illuminate\Support\Arr::first($jobs, fn($job) => $job['id'] == $id);
-        dd($job);
-
-    return View('contact');
+    return view('jobs', ['job' => $job]);
 });
+
 
 Route::get('/contact', function () {
     return View('contact');
